f1563b5dad2b8ea864b418b245a380be34af98a5
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-04-24 12:34:49 +0530

    Added dependency for Kotlin-extensions. Added the Asteroid data class.

---
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/data/Asteroid.kt
@@ -0,0 +1,20 @@
+package com.gmail.apigeoneer.aesteroids.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ * Parcelable is a serialization mechanism provided by Android to pass complex data from one activity to another activity.
+ * In order to write an object to a Parcel, that object should implement the interface “Parcelable“.
+ */
+@Parcelize
+data class Asteroid(
+    val id: Long,
+    val codename: String,
+    val closeApproachDate: String,
+    val absoluteMagnitude: Double,
+    val estimateDiameter: Double,
+    val relativeVelocity: Double,
+    val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+): Parcelable
