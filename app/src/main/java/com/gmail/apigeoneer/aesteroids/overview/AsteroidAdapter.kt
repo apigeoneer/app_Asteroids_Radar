24d8e8928a26b64292af44c50637136aeb9aa9d1
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-19 12:06:32 +0530

    Made the AsteroidViewHolder & OnClickListener classes as inner class of AsteroidAdapter

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/overview/AsteroidAdapter.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/overview/AsteroidAdapter.kt
@@ -11,7 +11,7 @@ import com.gmail.apigeoneer.aesteroids.databinding.AsteroidItemBinding
 
 class AsteroidAdapter(
     val onClickListener: OnClickListener
-) : androidx.recyclerview.widget.ListAdapter<Asteroid, AsteroidViewHolder>(DiffCallback){
+) : androidx.recyclerview.widget.ListAdapter<Asteroid, AsteroidAdapter.AsteroidViewHolder>(DiffCallback){
 
     // We make this a companion object cuz we want this in the namespace of our class,
     // but it doesn't need a reference to our class
@@ -37,15 +37,15 @@ class AsteroidAdapter(
             onClickListener.onClick(asteroid)
         }
     }
-}
 
-class AsteroidViewHolder(private val binding: AsteroidItemBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(asteroid: Asteroid) {
-        binding.asteroids = asteroid
-        binding.executePendingBindings()
+    inner class AsteroidViewHolder(private val binding: AsteroidItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(asteroid: Asteroid) {
+            binding.asteroids = asteroid
+            binding.executePendingBindings()
+        }
     }
-}
 
-class OnClickListener(val clickListener: (asteroid: Asteroid) -> Unit) {
-    fun onClick(asteroid: Asteroid) = clickListener(asteroid)
+    class OnClickListener(val clickListener: (asteroid: Asteroid) -> Unit) {
+        fun onClick(asteroid: Asteroid) = clickListener(asteroid)
+    }
 }
