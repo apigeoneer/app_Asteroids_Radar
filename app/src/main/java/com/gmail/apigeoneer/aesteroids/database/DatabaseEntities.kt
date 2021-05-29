6d6fecc043f3d0865f46ce2d05a31312a4ce44d5
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-29 23:23:31 +0530

    Added DatabaseEntities class

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/database/DatabaseEntities.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/database/DatabaseEntities.kt
@@ -1,4 +1,33 @@
 package com.gmail.apigeoneer.aesteroids.database
 
-import com.squareup.moshi.Json
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gmail.apigeoneer.aesteroids.data.Asteroid
 
+@Entity
+data class DatabaseAsteroid constructor(
+        @PrimaryKey
+        val id: Long,
+        val codeName: String,
+        val closeApproachDate: String,
+        val absMagnitude: Double,
+        val estDiaMax: Double,
+        val isHazardous: Boolean,
+        val relativeVelocity: Double,
+        val distanceFromEarth: Double,
+)
+
+fun List<DatabaseAsteroid>.asDomainModel(): List<Asteroid> {
+    return map {
+        Asteroid(
+                id = it.id,
+                codeName = it.codeName,
+                closeApproachDate = it.closeApproachDate,
+                absMagnitude = it.absMagnitude,
+                estDiaMax = it.estDiaMax,
+                isHazardous = it.isHazardous,
+                relativeVelocity = it.relativeVelocity,
+                distanceFromEarth = it.distanceFromEarth
+        )
+    }
+}
