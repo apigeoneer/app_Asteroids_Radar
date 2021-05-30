7fa26c57b7dce607b0d91f7dc7c19504c92406d6
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-30 10:33:30 +0530

    Created getDatabase() & AsteroidsDatabase abstract class

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/database/Room.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/database/Room.kt
@@ -1,8 +1,8 @@
 package com.gmail.apigeoneer.aesteroids.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 
 @Dao
 interface AsteroidDao {
@@ -10,3 +10,23 @@ interface AsteroidDao {
     fun getAsteroids(): LiveData<List<DatabaseAsteroid>>
 }
 
+@Database(entities = [AsteroidsDatabase::class], version = 1)
+abstract class AsteroidsDatabase: RoomDatabase() {
+    abstract val asteroidDao: AsteroidDao
+}
+
+// var for our singleton
+private lateinit var INSTANCE: AsteroidsDatabase
+
+fun getDatabase(context: Context): AsteroidsDatabase {
+    // Made code synchronized so its thread safe
+    synchronized(AsteroidsDatabase::class.java) {
+        // Check whether the database has been initialized
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+            AsteroidsDatabase::class.java,
+            "asteroids").build()
+        }
+    }
+    return INSTANCE
+}
