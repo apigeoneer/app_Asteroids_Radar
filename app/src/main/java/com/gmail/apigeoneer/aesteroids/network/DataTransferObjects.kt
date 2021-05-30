c5e45881737d661ec435cf46aed75011837dfbcd
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-30 11:22:15 +0530

    Added table name & removed unnecessary code

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/network/DataTransferObjects.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/network/DataTransferObjects.kt
@@ -1,12 +1,12 @@
 package com.gmail.apigeoneer.aesteroids.network
 
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.gmail.apigeoneer.aesteroids.database.DatabaseAsteroid
 
-data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
-
+@Entity(tableName = "asteroid_table")
 data class NetworkAsteroid constructor(
-        @PrimaryKey
+        @PrimaryKey(autoGenerate = true)
         val id: Long,
         val codeName: String,
         val closeApproachDate: String,
@@ -16,18 +16,3 @@ data class NetworkAsteroid constructor(
         val relativeVelocity: Double,
         val distanceFromEarth: Double,
 )
-
-fun NetworkAsteroidContainer.asDatabaseModel(): Array<DatabaseAsteroid> {
-    return asteroids.map {
-        DatabaseAsteroid (
-                id = it.id,
-                codeName = it.codeName,
-                closeApproachDate = it.closeApproachDate,
-                absMagnitude = it.absMagnitude,
-                estDiaMax = it.estDiaMax,
-                isHazardous = it.isHazardous,
-                relativeVelocity = it.relativeVelocity,
-                distanceFromEarth = it.distanceFromEarth
-        )
-    }.toTypedArray()
-}
