51e4536a29f7288dd75c3e13154d5c85c44afd31
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-05-30 11:43:35 +0530

    Created PictureOfTheDay DAO

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/database/Room.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/database/Room.kt
@@ -11,26 +11,34 @@ interface AsteroidDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(vararg asteroids: AsteroidEntity)
-
 }
 
-@Database(entities = [AsteroidsDatabase::class], version = 1)
-abstract class AsteroidsDatabase: RoomDatabase() {
-    abstract val asteroidDao: AsteroidDao
+@Dao
+interface PictureOfTheDayDao {
+    @Query("select * from picture_of_the_day_table")
+    fun getPictureOfTheDay(): LiveData<PictureOfTheDayEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg asteroids: PictureOfTheDayEntity)
 }
 
-// var for our singleton
-private lateinit var INSTANCE: AsteroidsDatabase
-
-fun getDatabase(context: Context): AsteroidsDatabase {
-    // Made code synchronized so its thread safe
-    synchronized(AsteroidsDatabase::class.java) {
-        // Check whether the database has been initialized
-        if (!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(context.applicationContext,
-            AsteroidsDatabase::class.java,
-            "asteroids").build()
-        }
-    }
-    return INSTANCE
-}
+//@Database(entities = [AsteroidsDatabase::class], version = 1)
+//abstract class AsteroidsDatabase: RoomDatabase() {
+//    abstract val asteroidDao: AsteroidDao
+//}
+//
+//// var for our singleton
+//private lateinit var INSTANCE: AsteroidsDatabase
+//
+//fun getDatabase(context: Context): AsteroidsDatabase {
+//    // Made code synchronized so its thread safe
+//    synchronized(AsteroidsDatabase::class.java) {
+//        // Check whether the database has been initialized
+//        if (!::INSTANCE.isInitialized) {
+//            INSTANCE = Room.databaseBuilder(context.applicationContext,
+//            AsteroidsDatabase::class.java,
+//            "asteroids").build()
+//        }
+//    }
+//    return INSTANCE
+//}
