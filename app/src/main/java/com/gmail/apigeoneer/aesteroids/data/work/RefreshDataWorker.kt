5a14eded1c3592ac6e418db48dbcb3ce770b0464
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-06-10 10:32:48 +0530

    Added PictureOfTheDay to the worker

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/data/work/RefreshDataWorker.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/data/work/RefreshDataWorker.kt
@@ -7,6 +7,7 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.gmail.apigeoneer.aesteroids.data.getDatabase
 import com.gmail.apigeoneer.aesteroids.data.repositories.AsteroidRepository
+import com.gmail.apigeoneer.aesteroids.data.repositories.PictureOfTheDayRepository
 import retrofit2.HttpException
 
 /**
@@ -22,9 +23,11 @@ class RefreshDataWorker(appContext: Context, params: WorkerParameters)
     @RequiresApi(Build.VERSION_CODES.O)
     override suspend fun doWork(): Result {
         val database = getDatabase(applicationContext)
-        val repository = AsteroidRepository(database)
+        val asteroidRepository = AsteroidRepository(database)
+        val pictureOfTheDayRepository = PictureOfTheDayRepository(database)
         return try {
-            repository.refreshAsteroids()
+            asteroidRepository.refreshAsteroids()
+            pictureOfTheDayRepository.refreshPictureOfTheDay()
             Result.success()
         } catch (e: HttpException) {
             Result.retry()
