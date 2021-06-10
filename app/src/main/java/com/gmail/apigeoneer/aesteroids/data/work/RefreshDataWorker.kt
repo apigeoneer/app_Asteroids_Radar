fbeb62015e11d165afa6f66bf14675845aec339d
Chitranshi Srivastava <apigeoneer@gmail.com>
2021-06-10 09:32:09 +0530

    Added WorkerParameters argument to the RefreshDataWorker class

--- app/src/main/java/com/gmail/apigeoneer/aesteroids/data/work/RefreshDataWorker.kt
+++ app/src/main/java/com/gmail/apigeoneer/aesteroids/data/work/RefreshDataWorker.kt
@@ -1,9 +1,10 @@
 package com.gmail.apigeoneer.aesteroids.data.work
 
 import android.content.Context
+import androidx.work.WorkerParameters
 
 /**
  * Pre-fetch data when the app is in the background
  */
-class RefreshDataWorker(appContext: Context) {
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) {
 }
